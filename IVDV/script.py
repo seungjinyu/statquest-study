0293dda4f378ecd90fbffea8fb97ee33fd7fa261
seungjinyu <seungjinyu93@gmail.com>
2024-02-26 16:31:08 +0900

    ch to eng and added comment on readme

--- IVDV/script.py
+++ IVDV/script.py
@@ -1,30 +1,30 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-# 독립 변수 (입력 데이터)
+# Independent variables (input data)
 X = np.array([[1], [2], [3], [4], [5]])
 
-# 종속 변수 (출력 데이터)
+# Dependent variables (output data)
 y = np.array([2, 3, 4, 5, 6])
 
-# 선형 회귀 모델 생성 및 학습
+# Create and train a linear regression model
 model = LinearRegression()
 model.fit(X, y)
 
-# 새로운 독립 변수 값에 대한 종속 변수 예측
+# Predict dependent variables for new independent variables
 new_X = np.array([[6], [7], [8]])
 predictions = model.predict(new_X)
 
-# 결과 출력
-print("독립 변수 (입력 데이터):")
+# Output
+print("Independent variables (input data):")
 print(X)
 
-print("\n종속 변수 (출력 데이터):")
+print("\nDependent variables (output data):")
 print(y)
 
-print("\n학습된 선형 회귀 모델의 계수 (기울기):", model.coef_[0])
-print("학습된 선형 회귀 모델의 절편:", model.intercept_)
+print("\nCoefficients (slope) of the trained linear regression model:", model.coef_[0])
+print("Intercept of the trained linear regression model:", model.intercept_)
 
-print("\n새로운 독립 변수 값에 대한 종속 변수 예측:")
+print("\nPredicted dependent variables for new independent variables:")
 for i, x in enumerate(new_X):
-    print("독립 변수:", x[0], "| 예측된 종속 변수:", predictions[i])
+    print("Independent variable:", x[0], "| Predicted dependent variable:", predictions[i])
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Statquest study repo
 
+I am just studying statquest and trying to understand the concept by python code
+
+Using chat gpt for faster coding.
